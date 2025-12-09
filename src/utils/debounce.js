f39d5b093a66acd72959d276a71d0ef06130755e
kingone <1069894848@qq.com>
2025-12-09 14:52:44 +0800

    防抖（模块-可配置）

--- src/utils/debounce.js
+++ src/utils/debounce.js
@@ -1,35 +1,38 @@
-export default function debounce2(func, wait, immediate) {
+export default function debounce2(func, wait, { leading = true, trailing = true } = {}) {
   //防御性编程：检查传入的是否是函数
   if (typeof func !== 'function') {
     throw new TypeError('debounce expected a function')
   }
   let timer = null
   let result = null
+  let lastArgs
+  let lastThis
   const debounced = function (...args) {
-    let context = this
+    lastArgs = args
+    lastThis = this
     if (timer) clearTimeout(timer)
 
-    if (immediate) {
-      //立即执行
-      const callNow = !timer
-      timer = setTimeout(() => {
-        timer = null
-      }, wait)
-      if (callNow) {
-        result = func.apply(context, args)
-      }
-    } else {
-      //常规延迟执行
-      timer = setTimeout(() => {
-        result = func.apply(context, args)
-      }, wait)
+    if (leading && !timer) {
+      result = func.apply(lastThis, lastArgs)
+      lastArgs = null
+      lastThis = null
     }
+    timer = setTimeout(() => {
+      timer = null
+      if (trailing && lastArgs) {
+        result = func.apply(lastThis, lastArgs)
+        lastArgs = null
+        lastThis = null
+      }
+    }, wait)
     return result
   }
   debounced.cancel = function () {
     //取消防抖
     clearTimeout(timer)
     timer = null
+    lastArgs = null
+    lastThis = null
   }
   return debounced
 }
