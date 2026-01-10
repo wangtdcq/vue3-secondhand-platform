352f2f6950ec61c8ebfb8b288875cded80e34843
kingone <1069894848@qq.com>
2026-01-10 21:42:50 +0800

    chose:修改过去漏import的错误

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+//elementplus按需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 import { visualizer } from 'rollup-plugin-visualizer'
-// ... 其他 import 保持不变
 
 export default defineConfig({
   plugins: [
