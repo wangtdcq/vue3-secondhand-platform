78a73e9355315e6f2c860c465de948f54fb7de89
kingone <1069894848@qq.com>
2026-01-10 23:42:12 +0800

    fix:注释掉了不用的DevTolls的import

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+//import vueDevTools from 'vite-plugin-vue-devtools'
 
 //elementplus按需导入
 import AutoImport from 'unplugin-auto-import/vite'
