1fef2f0f2d4eb1df2c27bc4acefc97ba86646ae6
kingone <1069894848@qq.com>
2026-01-24 16:56:19 +0800

    refactor：修改 pc Router 配置

--- src/pages/pc/router/index.js
+++ src/pages/pc/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Layout from '@/views/Layout/index.vue'
+import Layout from '@/pages/pc/views/Layout/index.vue'
 // import Login from '@/views/Login/index.vue'
-import Home from '@/views/Home/index.vue'
+import Home from '@/pages/pc/views/Home/index.vue'
 // import Category from '@/views/Category/index.vue'
 // import Member from '@/views/Member/index.vue'
 // import denglu from '@/views/Login/component/denglu.vue'
@@ -32,44 +32,47 @@ const router = createRouter({
           children: [
             {
               path: '',
-              component: () => import('@/views/Home/components/FilterPrice.vue'),
+              component: () => import('@/pages/pc/views/Home/components/FilterPrice.vue'),
             },
             {
               path: 'filtertime',
-              component: () => import('@/views/Home/components/FilterTime.vue'),
+              component: () => import('@/pages/pc/views/Home/components/FilterTime.vue'),
             },
           ],
         },
         {
           path: 'Category/:id',
-          component: () => import('@/views/Category/index.vue'),
+          component: () => import('@/pages/pc/views/Category/index.vue'),
           meta: { requiresAuth: true }, //设定访问权限（只有先登录才能访问）
         },
         {
           path: 'detail/:id',
-          component: () => import(/*webpackChunkName:"detail"*/ '@/views/Detail/index.vue'),
+          component: () =>
+            import(/*webpackChunkName:"detail"*/ '@/pages/pc/views/Detail/index.vue'),
           meta: { requiresAuth: true },
         },
         {
           path: 'talk',
           component: () =>
-            import(/*webpackChunkName:"detail"*/ '@/views/Detail/components/Talk.vue'),
+            import(/*webpackChunkName:"detail"*/ '@/pages/pc/views/Detail/components/Talk.vue'),
           meta: { requiresAuth: true },
         },
         {
           path: 'cartlist',
-          component: () => import(/*webpackChunkName:"cartlist"*/ '@/views/CartList/index.vue'),
+          component: () =>
+            import(/*webpackChunkName:"cartlist"*/ '@/pages/pc/views/CartList/index.vue'),
           meta: { requiresAuth: true },
         },
         {
           path: 'checkout',
-          component: () => import(/*webpackChunkName:"checkout"*/ '@/views/Checkout/index.vue'),
+          component: () =>
+            import(/*webpackChunkName:"checkout"*/ '@/pages/pc/views/Checkout/index.vue'),
           meta: { requiresAuth: true },
         },
         {
           path: 'search',
           component: () =>
-            import(/*webpackChunkName:"search"*/ '@/views/Layout/components/Search.vue'),
+            import(/*webpackChunkName:"search"*/ '@/pages/pc/views/Layout/components/Search.vue'),
           props: (route) => ({
             searchParam: route.query.param || '',
           }),
@@ -79,49 +82,57 @@ const router = createRouter({
     },
     {
       path: '/login',
-      component: () => import(/* webpackChunkName:"login-group" */ '@/views/Login/index.vue'),
+      component: () =>
+        import(/* webpackChunkName:"login-group" */ '@/pages/pc/views/Login/index.vue'),
       children: [
         {
           path: '',
           component: () =>
-            import(/* webpackChunkName:"login-group" */ '@/views/Login/component/denglu.vue'),
+            import(
+              /* webpackChunkName:"login-group" */ '@/pages/pc/views/Login/component/denglu.vue'
+            ),
         },
         {
           path: 'zhuce',
           component: () =>
-            import(/* webpackChunkName:"login-group" */ '@/views/Login/component/zhuce.vue'),
+            import(
+              /* webpackChunkName:"login-group" */ '@/pages/pc/views/Login/component/zhuce.vue'
+            ),
         },
       ],
     },
     {
       path: '/member',
-      component: () => import(/*webpackChunkName:"member-group" */ '@/views/Member/index.vue'),
+      component: () =>
+        import(/*webpackChunkName:"member-group" */ '@/pages/pc/views/Member/index.vue'),
       meta: { requiresAuth: true },
       children: [
         {
           path: '',
           component: () =>
-            import(/*webpackChunkName:"member-group" */ '@/views/Member/components/userInfo.vue'),
+            import(
+              /*webpackChunkName:"member-group" */ '@/pages/pc/views/Member/components/userInfo.vue'
+            ),
         },
         {
           path: 'order',
           component: () =>
             import(
-              /*webpackChunkName:member-otherGroup"*/ '@/views/Member/components/userOrder.vue'
+              /*webpackChunkName:member-otherGroup"*/ '@/pages/pc/views/Member/components/userOrder.vue'
             ),
         },
         {
           path: 'shell',
           component: () =>
             import(
-              /*webpackChunkName:member-otherGroup"*/ '@/views/Member/components/userSell.vue'
+              /*webpackChunkName:member-otherGroup"*/ '@/pages/pc/views/Member/components/userSell.vue'
             ),
         },
         {
           path: 'public',
           component: () =>
             import(
-              /*webpackChunkName:member-otherGroup"*/ '@/views/Member/components/userPublic.vue'
+              /*webpackChunkName:member-otherGroup"*/ '@/pages/pc/views/Member/components/userPublic.vue'
             ),
         },
       ],
