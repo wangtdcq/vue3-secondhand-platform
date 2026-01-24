37cb70b83717933551fdd3757957ca845913dd32
kingone <1069894848@qq.com>
2026-01-24 17:33:39 +0800

    feature：在 mobile/main.js 中注册Pinia

--- src/pages/mobile/main.js
+++ src/pages/mobile/main.js
@@ -5,11 +5,13 @@ import router from './router'
 // import Vant from 'vant' // 移动端常用 UI 库
 // import 'vant/lib/index.css'
 // import 'amfe-flexible' // 如果你使用 rem 适配方案，需要引入这个
+import { createPinia } from 'pinia'
 
 import './styles/main.css'
 
 const app = createApp(App)
 
+app.use(createPinia())
 app.use(router)
 // app.use(Vant)
 
