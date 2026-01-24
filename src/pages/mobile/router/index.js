1179dcb4c58495af3c6eb6d5fcb81f41e7fef08a
kingone <1069894848@qq.com>
2026-01-24 19:53:44 +0800

    factor：配置 mobile/router

--- src/pages/mobile/router/index.js
+++ src/pages/mobile/router/index.js
@@ -1,8 +1,20 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
+import Layout from '@/pages/mobile/views/Layout/index.vue'
 const router = createRouter({
   // 使用 Hash 模式,直接就能跑,但不美观
   history: createWebHashHistory(),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'Layout',
+      component: Layout,
+    },
+    {
+      path: '/member',
+      component: () =>
+        import(/*webpackChunkName:"member" */ '@/pages/mobile/views/Member/index.vue'),
+    },
+  ],
 })
 export default router
