cb7ce93994cb84391f499cdaa2db32da8f931a4d
kingone <1069894848@qq.com>
2026-01-24 12:06:45 +0800

    refactor：使用Hash模式配置Router的base

--- src/pages/mobile/router/index.js
+++ src/pages/mobile/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory('/mobile/'),
-
+  // 使用 Hash 模式,直接就能跑,但不美观
+  history: createWebHashHistory(),
   routes: [],
 })
 export default router
